328fd570d7a0b69b741d4e18b33d71499b9b4d35
Keven C <keven_jkc@hotmail.com>
2021-06-08 14:17:22 -0300

    conexão com mongodb, prefixo configurado, token ocultado

---
+++ commands/say.js
@@ -0,0 +1,8 @@
+
+module.exports.run = async(client, message, args) => {
+
+  const sayMessage = args.join(" ");
+  message.delete().catch(() =>{})
+  message.channel.send(sayMessage);
+
+}
--- index.js
+++ index.js
@@ -1,22 +1,50 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const mongoose = require('mongoose');
+const TOKEN = process.env.TOKEN;
+const config = require("./config.json");
 
-const TOKEN ='ODUxMTIwOTg3MzQ0NjAxMDk5.YLzqMA.zPC5wIBLpI09dtufSpN2w_PJCjY';
+require('dotenv').config()
+
+
+//Conexão com mongodb
+mongoose.connect('mongodb+srv://kevenjkc:datafag123@cluster0.pheev.mongodb.net/Data', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => console.log("conectado ao mongodb"));
+
+
 
 client.on("ready",() => {
-
     console.log('Funcionando pae!');
-
 });
 
 client.on("message", (msg) =>{
-
-   
-   
     if(msg.content.toLowerCase() === "Oie".toLowerCase()) {
         msg.reply('Salve!');
     }
 
 });
+
+client.on("message", async message =>{
+if(message.author.bot) return;
+if (message.channel.type === "dm") return;
+if (!message.content.startsWith(config.prefix)) return;
+if (message.content.startsWith (`<@!${client.user.id}`) || message.content.startsWith(`<@${client.user.id}`)) return;
+
+let args = message.content.split(" ").slice(1);
+let command = message.content.split(" ")[0];
+command = command.slice(config.prefix.length);
+
+// Leitura dos comandos na pasta comandos
+try {
+    let commandFile = require(`./commands/${command}.js`);
+    delete require.cache[require.resolve(`./commands/${command}.js`)];
+    return commandFile.run(client, message, args);
+}  catch (err) {
+    console.error("Erro" + err);
+}
+
+})
 
 client.login(TOKEN);
